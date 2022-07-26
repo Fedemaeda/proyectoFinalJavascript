3ebe16136cbd5efd179df5f454aab5aa17f97df7
Federico Maeda <fedemaeda@gmail.com>
2022-07-26 17:19:11 -0300

    dom

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+const tarjetasHtml = PRODUCTOS.productos.reduce((acc, elemento, i) => {   
+
+        return acc = acc + `    
+            <div class="tarjeta">
+                <div class="img-container">
+                    <img src=${elemento.img} alt=${elemento.name}>
+                </div>   
+                <p>
+                    ${elemento.name}
+                </p> 
+                   
+            </div>
+        `       
+},"")
+
+console.log(tarjetasHtml)
+
+const contenedorTarjetas = document.querySelector(".contenedor-tarjetas")
+
+console.log(contenedorTarjetas)
+
+contenedorTarjetas.innerHTML = tarjetasHtml
+
---
+++ productos.js
@@ -0,0 +1,102 @@
+const PRODUCTOS  = {
+  "productos": [{
+    "id": 1,
+    "num": "001",
+    "name": "ACEITE DE CHIA",
+    "img": "https://lasniatas.netlify.app/img/aceitedechia.jpeg",
+   
+  
+  }, {
+    "id": 2,
+    "num": "002",
+    "name": "ACEITE DE OLIVA",
+    "img": "https://lasniatas.netlify.app/img/aceitedeolivaextravirgen.jpeg",
+    
+  }, {
+    "id": 3,
+    "num": "003",
+    "name": "AJÍES",
+    "img": "https://lasniatas.netlify.app/img/ajies.jpeg",
+
+  }, {
+    "id": 4,
+    "num": "004",
+    "name": "AJO EN POLVO",
+    "img": "https://lasniatas.netlify.app/img/ajoenpolvo.jpeg",
+
+  }, {
+    "id": 5,
+    "num": "005",
+    "name": "BEBIDA VEGETAL DE ALMENDRA Y VAINILLA",
+    "img": "https://lasniatas.netlify.app/img/bebidavegetalalmedravainilla.jpeg",
+
+  }, {
+    "id": 6,
+    "num": "006",
+    "name": "BEBIDA VEGETAL DE ALMENDRA Y COCO",
+    "img": "https://lasniatas.netlify.app/img/bebidavegetalalmendraycoco.jpeg",
+
+  }, {
+    "id": 7,
+    "num": "007",
+    "name": "BEBIDA VEGETAL SABOR CHOCOLATADA",
+    "img": "https://lasniatas.netlify.app/img/bebidavegetalchocolatada.jpeg",
+
+  }, {
+    "id": 8,
+    "num": "008",
+    "name": "COCO RALLADO",
+    "img": "https://lasniatas.netlify.app/img/cocorallado.jpeg",
+
+  }, {
+    "id": 9,
+    "num": "009",
+    "name": "MERMELADA DE ZANAHORIA, PERA Y JENGIBRE",
+    "img": "https://lasniatas.netlify.app/img/mermemanzanaperajengibre.jpeg",
+
+  }, {
+    "id": 10,
+    "num": "010",
+    "name": "MERMELADA DE NARANJA",
+    "img": "https://lasniatas.netlify.app/img/mermenaranja.jpeg",
+
+  }, {
+    "id": 11,
+    "num": "011",
+    "name": "MIEL",
+    "img": "https://lasniatas.netlify.app/img/miel.jpeg",
+  }, {
+
+    "id": 12,
+    "num": "012",
+    "name": "ORÉGANO DE MENDOZA",
+    "img": "https://lasniatas.netlify.app/img/oreganodemendoza.jpeg",
+  }, {
+
+    "id": 13,
+    "num": "013",
+    "name": "PAN RÚSTICO",
+    "img": "https://lasniatas.netlify.app/img/panrustico.jpeg",
+
+  }, {
+    "id": 14,
+    "num": "014",
+    "name": "PROVENZAL",
+    "img": "https://lasniatas.netlify.app/img/provenzal.jpeg",
+  }, {
+
+    "id": 15,
+    "num": "015",
+    "name": "SAL DEL HIMALAYA",
+    "img": "https://lasniatas.netlify.app/img/saldelhimalaya.jpeg",
+  }, {
+
+    "id": 16,
+    "num": "016",
+    "name": "SEMILLAS DE QUINOA",
+    "img": "https://lasniatas.netlify.app/img/semillasquinoa.jpeg",
+
+  
+  } ]
+}
+;;
